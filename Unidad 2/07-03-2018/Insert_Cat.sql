9387f3f33074e3c591a95e385bcbdb43b17607f6
carlosvm24 <carlos.molina@cetys.edu.mx>
2018-03-08 23:52:52 -0800

    Modificacion de tablas y algunas consultas

---
+++ Unidad 2/07-03-2018/Insert_Cat.sql
@@ -0,0 +1,17 @@
+INSERT INTO ICEICEBABY.CATEGORY
+VALUES(ICEICEBABY.CATEGORY_SEQ.nextval, 'Literature', 'The Nobel Prize in Literature');
+
+INSERT INTO ICEICEBABY.CATEGORY
+VALUES(ICEICEBABY.CATEGORY_SEQ.nextval, 'Physics', 'The Nobel Prize in Physics');
+
+INSERT INTO ICEICEBABY.CATEGORY
+VALUES(ICEICEBABY.CATEGORY_SEQ.nextval, 'Peace', 'The Nobel Peace Prize');
+
+INSERT INTO ICEICEBABY.CATEGORY
+VALUES(ICEICEBABY.CATEGORY_SEQ.nextval, 'Medicine', 'The Nobel Prize in Physiology or Medicine');
+
+INSERT INTO ICEICEBABY.CATEGORY
+VALUES(ICEICEBABY.CATEGORY_SEQ.nextval, 'Chemistry', 'The Nobel Prize in Chemistry');
+
+INSERT INTO ICEICEBABY.CATEGORY
+VALUES(ICEICEBABY.CATEGORY_SEQ.nextval, 'Economics', 'The Prize in Economic Sciences');
